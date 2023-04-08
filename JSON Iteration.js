5d090e30ce2c70904b18400c20520ba4a61508d9
vignesh-balaji4535 <127295249+vignesh-balaji4535@users.noreply.github.com>
2023-04-08 10:22:43 +0530

    Create JSON Iteration.js

---
+++ JSON Iteration.js
@@ -0,0 +1,56 @@
+ //1.iterate over a prop of person using for-in loop
+
+//const person={
+  //name:"balaji",
+  //age:21,
+  //course:"Diploma",
+  //hobbies:["playing","youtube","coding"],
+  //address:{
+  //village:"madhupatti",
+   //post:"settikarai",
+   //city:"dharmapuri",
+  //pincode:636704,
+//}
+//}
+
+  //for(let prop in person){
+    //if(prop==="hobbies"||prop==="address")//printing a specific prop and values
+    //console.log(prop,":",person[prop])
+ // }
+
+// 2.iterate over a prop1 of person using for-of loop
+
+//for(let prop1 of Object.keys(person)){ //Object.key method
+  //console.log(prop1,":",person[prop1])
+//}
+
+//for(let [keys,values] of Object.entries(person)){//Object.entites method
+  
+  //console.log(keys,":",values)
+//}
+  //array iteration in for-of loop
+//let fruits=["apple","orange","banana","pineapple"];
+
+//for(let fruit of fruits){
+//console.log(fruit)
+//}
+
+// 3 . for loop iteration in numbers
+
+
+//for(let i=0;i<5;i++){
+  //console.log(`number${i}`)
+//}
+
+// array iteration over for loop
+//let fruits=["apple","orange","banana","pineapple"];
+//for(let i=0; i<fruits.length;i++ ){
+  //console.log("index:",i,"=",fruits[i])
+//} 
+
+// 4. for each loop
+
+//let numbers = [1, 2, 3, 4];
+//numbers.forEach(function(number) {
+ // console.log(number);
+//});
