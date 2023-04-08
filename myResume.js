6e94d0e9f1c2e7f0ce20d6c59bca2e0a69eaefae
vignesh-balaji4535 <127295249+vignesh-balaji4535@users.noreply.github.com>
2023-04-08 10:38:02 +0530

    Create myResume.js

---
+++ myResume.js
@@ -0,0 +1,28 @@
+
+const myResume={
+  "name":"Vignesh Balaji",
+  "E-mail":"vigneshbalaji4535@gmail.com",
+  "mobile":"902527xxxx",
+  "father-Name":"Aranganathan",
+
+  "Qulification":{
+    "SSCL":"70%",
+    "HSC":"-",
+    "DIPLOMA":"80.5%"
+  },
+  
+  "Skill":{
+    1:"MERN stack developer",
+    2:"MasterCAM",
+    3:"CNC Programming"
+  },
+
+  "address":{
+    "village":"Madhupatti",
+    "post":"Settikarai",
+    "disctrit":"Dharmapuri"
+  },
+
+  "place":"dharmapuri"
+
+}
